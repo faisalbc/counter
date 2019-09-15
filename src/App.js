d1e59fb7c215cb5525a9aaf8a9d58fbca3b4185b
Faisal Md <s1900167@edu.bc.fi>
2019-09-16 00:12:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React, { Component} from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-const { useState } = React
-      const { render } = ReactDOM
-      const App = () => {
-        const [count, setCount] = useState(0)
-        const increase = () => setCount(c => ++c)
-        const reset = () => setCount(0)
-        const decrease = () => setCount(c => --c)
-        const isEven = count % 2 === 0
-        return (
-          <div id="app">
-            <div
-              id="circle"
-              onClick={increase}
-              style={{ background: isEven ? 'skyblue' : 'red' }}
-            >
-              {count}
-            </div>
-            <div id="buttons">
-              <button onClick={increase}>Increase</button>
-              <button onClick={decrease}>Decrease</button>
-              <button onClick={reset}>Reset</button>
-            </div>
-          </div>
-        )
-      }
-      render(<App />, document.querySelector('#root'))
-export default App;
+export default function App() {
+
+  const [count, setCount] = useState(0) // here I have mentioned variable for increase and decrease 
+
+  const increase = () => setCount(c => c +1) // it will add 1
+
+  const reset = () => setCount(0) // finction is zero
+
+  const decrease = () => setCount(c => --c) // decrease 1
+
+  const isEven = count % 2 === 0
+
+  return (
+    <div id="app">
+      <div
+        id="circle"
+        onClick={increase}
+        style={{ background: isEven ? 'skyblue' : 'red' }} // for the color change
+      >
+        {count}
+      </div>
+      <div id="buttons">
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  )
+}
